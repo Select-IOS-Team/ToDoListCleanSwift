54eca5c125c2eff2fdab42e91c5f7926ff3eb1f5
Evgeni Meleshin <evgenimeleshin@igmail.com>
2023-03-15 22:48:52 +0300

    Добавлен пакет TaskManagerPackage, в который вынесена бизнес-логика, а также тесты для нее. Внутри проекта бизнеса-логика и тесты удалены

--- ToDoListCleanSwift/TasksScene/Managers/TasksDependenciesBuilder.swift
+++ ToDoListCleanSwift/TasksScene/Managers/TasksDependenciesBuilder.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import TaskManagerPackage
 
 /// Класс сборщика
 enum TasksDependenciesBuilder {
--- ToDoListCleanSwift/TasksScene/TasksInteractor.swift
+++ ToDoListCleanSwift/TasksScene/TasksInteractor.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import TaskManagerPackage
 
 /// Протокол интерактора
 protocol ITasksInteractor: AnyObject {
