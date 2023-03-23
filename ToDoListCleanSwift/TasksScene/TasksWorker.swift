bc5d72da84706b3b6e57c4afcad27a81e7e1fd5b
Evgeni Meleshin <evgenimeleshin@igmail.com>
2023-03-23 15:00:02 +0300

    Корректировка текста в параметра метода

--- ToDoListCleanSwift/TasksScene/TasksWorker.swift
+++ ToDoListCleanSwift/TasksScene/TasksWorker.swift
@@ -10,9 +10,9 @@ import ToDoListBusinessLogic
 
 /// Воркер, подготавливающий модели данных о секциях и задачах для презентера
 protocol ITasksWorker {
-	/// Преобразует данные о секциях и из задачах из массива кортежей в модель TaskModel.Response.
-	/// - Parameter data: Массив кортежей с параметрами: sectionType - тип секции и tasks - массив задач в секции
-	/// - Returns: Модель TaskModel.Response, содержащая переданные на вход данные
+	/// Преобразует данные о секциях и из задачах из массива кортежей в модель `TaskModel.Response`.
+	/// - Parameter data: Массив кортежей с параметрами: `sectionType` - тип секции и `tasks` - массив задач в секции
+	/// - Returns: Модель `TaskModel.Response`, содержащая переданные на вход данные
 	func convertToTaskModelResponse(data: [(sectionType: SectionType, tasks: [Task])]) -> TaskModel.Response
 }
 
