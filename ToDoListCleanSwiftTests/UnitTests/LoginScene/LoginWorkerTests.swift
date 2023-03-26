80b5a5bc9a687ee68dbe06ebf0a5c939eadbdb19
Evgeny Novgorodov <e.novgorodov@tinkoff.ru>
2023-03-26 21:35:56 +0600

    Правки кодстайла

--- ToDoListCleanSwiftTests/UnitTests/LoginScene/LoginWorkerTests.swift
+++ ToDoListCleanSwiftTests/UnitTests/LoginScene/LoginWorkerTests.swift
@@ -10,40 +10,61 @@ import XCTest
 
 final class LoginWorkerTests: XCTestCase {
 
-	private let validLoginDTO = LoginDTO(success: 1, login: "1")
-	private let invalidPasswordLoginDTO = LoginDTO(success: 0, login: "1")
-	private let invalidLoginLoginDTO = LoginDTO(success: 0, login: "0")
-	private let invalidDataLoginDTO = LoginDTO(success: 0, login: "0")
+	// MARK: - Tests
 
 	func test_login_withValidData_shouldEqualWithValidLoginDTO() {
+		// arrange
 		let sut = LoginWorker()
 
+		// act
 		let result = sut.login(login: "1", password: "1")
 
-		XCTAssertEqual(result, validLoginDTO, "Не соответствует валидному ответу")
+		// assert
+		XCTAssertEqual(result, StubData.validLoginDTO, "Не соответствует валидному ответу")
 	}
 
 	func test_login_withInvalidPassword_shouldEqualWithinvalidPasswordLoginDTO() {
+		// arrange
 		let sut = LoginWorker()
 
+		// act
 		let result = sut.login(login: "1", password: "0")
 
-		XCTAssertEqual(result, invalidPasswordLoginDTO, "Не соответствует ответу для неверного пароля")
+		// assert
+		XCTAssertEqual(result, StubData.invalidPasswordLoginDTO, "Не соответствует ответу для неверного пароля")
 	}
 
 	func test_login_withInvalidLogin_shouldEqualWithInvalidLoginLoginDTO() {
+		// arrange
 		let sut = LoginWorker()
 
+		// act
 		let result = sut.login(login: "0", password: "1")
 
-		XCTAssertEqual(result, invalidLoginLoginDTO, "Не соответствует ответу для неверного логина")
+		// assert
+		XCTAssertEqual(result, StubData.invalidLoginLoginDTO, "Не соответствует ответу для неверного логина")
 	}
 
 	func test_login_withInvalidAllData_shouldEqualWithInvalidDataLoginDTO() {
+		// arrange
 		let sut = LoginWorker()
 
+		// act
 		let result = sut.login(login: "0", password: "0")
 
-		XCTAssertEqual(result, invalidDataLoginDTO, "Не соответствует ответу для неверных логина и пароля")
+		// assert
+		XCTAssertEqual(result, StubData.invalidDataLoginDTO, "Не соответствует ответу для неверных логина и пароля")
+	}
+}
+
+// MARK: - TestData
+
+private extension LoginWorkerTests {
+
+	enum StubData {
+		static let validLoginDTO = LoginDTO(success: 1, login: "1")
+		static let invalidPasswordLoginDTO = LoginDTO(success: 0, login: "1")
+		static let invalidLoginLoginDTO = LoginDTO(success: 0, login: "0")
+		static let invalidDataLoginDTO = LoginDTO(success: 0, login: "0")
 	}
 }
