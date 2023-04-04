479d3f681dc96166759400eb1d3c92d1cbaf6158
Evgeni Meleshin <evgenimeleshin@igmail.com>
2023-04-04 10:16:17 +0300

    Добавлен enum AccessibilityIdentifier с идентификаторами доступности для UI компонентов. Указаны идентификаторы доступности UI компонентам. Корректировки включения/отключения правил литера.

---
+++ ToDoListCleanSwift/AccessibilityIdentifier.swift
@@ -0,0 +1,18 @@
+//
+//  AccessibilityIdentifier.swift
+//  ToDoListCleanSwift
+//
+//  Created by Evgeni Meleshin on 04.04.2023.
+//
+
+/// Accessibility identifiers для UI компонентов
+enum AccessibilityIdentifier: String {
+
+	// MARK: - LoginScene
+	case loginTextField
+	case passwordTextField
+	case loginButton
+
+	// MARK: - TasksScene
+	case tasksTableView
+}
